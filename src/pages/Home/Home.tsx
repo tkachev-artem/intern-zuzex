48958350f69d03ef666c15c71b50006f1c89ae4b
artemtkachev <tkachev.tech@yandex.ru>
2025-06-04 00:22:21 +0300

    feat: донастройка

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,14 @@
 import "./styles/Home.scss"
 
 const Home = () => {
-  return <div>
+  return (
+    <div className="home_container">
+ 
+
     <div className="text-red-500 text-2xl font-bold">Home</div>
     <div className="home_title">Home</div>
-  </div>
+    </div>
+  )
 }
 
 export default Home
