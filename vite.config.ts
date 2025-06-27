6072c9715c513620f559a5a3628347ad0478c122
artemtkachev <tkachev.tech@yandex.ru>
2025-06-27 16:51:14 +0300

    feat: настройка деплоя на GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/it-lenta/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
